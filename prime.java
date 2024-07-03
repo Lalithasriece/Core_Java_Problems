4956be34cb7999704b51d37ef469fbe72157b3bc
Lalithasriece <144588804+Lalithasriece@users.noreply.github.com>
2024-07-03 19:49:41 +0530

    prime.java

--- prime.java
+++ prime.java
@@ -14,9 +14,14 @@ public class prime {
 
 public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
-    int num=sc.nextInt();
+    System.out.print("Enter the start value:");
+    int start=sc.nextInt();
+    System.out.print("Enter the end value:");
+    int end=sc.nextInt();
     int count=0;
-    for( num=1;num<=10;num++){
+    System.out.println("The prime numbers between "+start+" and "+end+" is :");
+    for(int num=start;num<=end;num++){
+       // System.out.println("The prime numbers between "+start+" and "+end+" is :");
         if(isPrime(num)){
             System.out.println(num);
             count++;
@@ -24,5 +29,17 @@ public static void main(String args[]){
     }
     System.out.println("The number of prime numbers is:"+count);
 }
-
 }
+
+/*OUTPUT:
+Enter the start value:5
+Enter the end value:20
+The prime numbers between 5 and 20 is :
+5
+7
+9
+11
+13
+17
+19
+The number of prime numbers is:7 */
